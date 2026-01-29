0240c5fc8c5b8809fe35fe121d40017c59c19559
oelaimar <oussama.el.aimar@gmail.com>
2026-01-29 11:18:59 +0100

    feat: add the middleware to web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,24 +6,23 @@ use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [CatalogController::class, 'index'])->name('catalog.index');
-Route::get('/products/{product}', [CatalogController::class, 'show'])->name('catalog.show');
-
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
 
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/products/{product}', [CatalogController::class, 'show'])->name('catalog.show');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
